ba6285a036ca16cd97b0f8859eccd352ac3d3ab9
URK21CS1098 <110011299+URK21CS1098@users.noreply.github.com>
2022-11-06 21:13:46 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,17 @@
 from typing import List
-
 def selectionSort(array, size) -> List[int]:
-  # Write your code here
-
-# Do not change the following code
+for y in range(len(array)):
+mid = y
+for j in range(y+1,len(array)):
+if array[mid] > array[j]:
+mid = j
+array[y], array[mid] = array[mid] , array[y]
+return array
 input_data = input()
 data = []
 for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
+if item.isnumeric():
+data.append(int(item))
+elif item.lstrip("-").isnumeric():
+data.append(int(item))
 print(selectionSort(data, len(data)))
